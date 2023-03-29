3d2bec325b00146799906c6b018a6625b85b3b49
Bhagbat S S Mohapatra <defenderbhagbat@gmail.com>
2023-03-29 23:54:08 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,31 +13,35 @@ const Footer = () => {
                             <p>Bindekhapur</p>
                             <p>soul society</p>
                         </div>
-                    <div className="phone">
-                        <h4>
-                        <FaPhone size={20} style={{ color: "#fff", marginRight: '2rem' }} />
-                        7894561230
-                        </h4>
-                    </div>
-                    <div className="email">
-                        <h4>
-                            <FaMailBulk size={20} style={{ color: "#fff", marginRight: '2rem' }} />
-                        soulripper@gmail.com
-                        </h4>
-                    </div>
+                        <div className="phone">
+                            <h4>
+                                <FaPhone size={20} style={{ color: "#fff", marginRight: '2rem' }} />
+                                7894561230
+                            </h4>
+                        </div>
+                        <div className="email">
+                            <h4>
+                                <FaMailBulk size={20} style={{ color: "#fff", marginRight: '2rem' }} />
+                                soulripper@gmail.com
+                            </h4>
+                        </div>
                     </div>
                 </div>
                 <div className="right">
                     <h2>About the Page</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi, libero ratione, quod repellendus asperiores odio illum, quis delectus in rerum! Quaerat atque, accusantium eos neque qui soluta repellat veniam.</p>
-                    <FaFacebook size={20} style={{ color: "#fff", marginRight: '2rem' }}></FaFacebook>
-                    <FaTwitter size={20} style={{ color: "#fff", marginRight: '2rem' }}></FaTwitter>
-                    <FaLinkedinIn size={20} style={{ color: "#fff", marginRight: '2rem' }}></FaLinkedinIn>
-                    <FaGithub size={20} style={{ color: "#fff", marginRight: '2rem' }}></FaGithub>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi, libero ratione,
+                        quod repellendus asperiores odio illum, quis delectus in rerum! Quaerat atque, accusantium eos
+                        neque qui soluta repellat veniam.</p>
+                    <div className="social">
+                        <FaFacebook size={30} style={{ color: "#fff", marginRight: '4rem' }} ></FaFacebook>
+                        <FaTwitter size={30} style={{ color: "#fff", marginRight: '4rem' }}></FaTwitter>
+                        <FaLinkedinIn size={30} style={{ color: "#fff", marginRight: '4rem' }}></FaLinkedinIn>
+                        <FaGithub size={30} style={{ color: "#fff", marginRight: '4rem' }}></FaGithub>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
